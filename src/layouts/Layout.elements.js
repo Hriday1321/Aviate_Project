724d69bbb6b15c42fe975f33773c041ba11bd31f
Hriday Gupta <hridaygupta100@hotmail.com>
2022-09-28 16:33:50 +0530

    moar base

---
+++ src/layouts/Layout.elements.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const DivC = styled.div`
+    display: flex;
+    height: 100%;
+    min-height: 100vh;
+`
+
+export const Side = styled.div`
+    flex: 0.2;
+    background-color: white;
+    color: white
+`
+
+export const Main = styled.div`
+    flex: 0.8;
+    background-color: #F4FDFE;
+    color: black;
+`
