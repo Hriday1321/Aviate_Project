ece86ffcfb0aa0f2dd4c7df8401d577725cb3cd9
Hriday Gupta <hridaygupta100@hotmail.com>
2022-09-29 16:06:33 +0530

    adding in some data

---
+++ src/utils/dummy.js
@@ -0,0 +1,7 @@
+export const dummy = [{
+    name: Netflix,
+    applied: new Date('08-22-2022'),
+    response: new Date('08-25-2022'),
+    cur_deadline: new Date('09-30-2022'),
+    task: 'Improve efficiency of the first submitted project'
+}]
