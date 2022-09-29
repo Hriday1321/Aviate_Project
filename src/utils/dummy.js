410a7b66a27625e2f803a34590f5bdb0d3f86145
Hriday Gupta <hridaygupta100@hotmail.com>
2022-09-29 16:20:40 +0530

    decided model

--- src/utils/dummy.js
+++ src/utils/dummy.js
@@ -1,7 +1,19 @@
 export const dummy = [{
     name: Netflix,
     applied: new Date('08-22-2022'),
-    response: new Date('08-25-2022'),
+    cur_date: new Date('09-17-2022'),
     cur_deadline: new Date('09-30-2022'),
-    task: 'Improve efficiency of the first submitted project'
+    task: 'Improve efficiency of the first submitted project',
+    timeline: [
+        {
+            task: 'Record an introductory video',
+            date: new Date('08-29-2022'),
+            deadline: new Date('09-05-2022')
+        },
+        {
+            task: 'Schedule Interview 1',
+            date: new Date('09-06-2022'),
+            deadline: new Date('09-13-2022')
+        }
+    ]
 }]
