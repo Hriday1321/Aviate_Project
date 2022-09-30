c4b65115ef73d0cf2006308b833d2fa439315e74
Hriday Gupta <hridaygupta100@hotmail.com>
2022-09-30 14:55:10 +0530

    moar werk

---
+++ src/components/Home/Home.elements.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Grid = styled.div`
+    display: grid;
+    grid-template-columns: 300px 300px 300px;
+    justify-content: space-evenly;
+    margin: 1%;
+    gap: 1rem;
+`;
+
+export const T = styled.div`
+    font-size: 2vw;
+    text-align: center;
+    padding: 0.5%;
+    height: 5vh;
+    background-color: white;
+    color: #777;
+`;
--- src/utils/dummy.js
+++ src/utils/dummy.js
@@ -1,5 +1,7 @@
 export const dummy = [{
     name: 'Netflix',
+    position: 'Junior SDE',
+    moni: 28,
     applied: new Date('08-22-2022'),
     cur_date: new Date('09-17-2022'),
     cur_deadline: new Date('09-30-2022'),
@@ -14,6 +16,27 @@ export const dummy = [{
             task: 'Schedule Interview 1',
             date: new Date('09-06-2022'),
             deadline: new Date('09-13-2022')
+        }
+    ]
+},
+{
+    name: 'Amazon',
+    position: 'SDE - 1',
+    moni: 18,
+    applied: new Date('08-24-2022'),
+    cur_date: new Date('09-22-2022'),
+    cur_deadline: new Date('10-02-2022'),
+    task: 'Run backend service through Lambda functions',
+    timeline: [
+        {
+            task: 'Record an introductory video',
+            date: new Date('08-29-2022'),
+            deadline: new Date('09-05-2022')
+        },
+        {
+            task: 'Schedule Interview 1',
+            date: new Date('09-10-2022'),
+            deadline: new Date('09-17-2022')
         }
     ]
 }]
